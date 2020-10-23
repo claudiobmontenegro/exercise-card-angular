59b3a883a07fc2438c18abf580a4980cbecae026
Claudio Montenegro <claudiobmontenegro@gmail.com>
2020-10-22 23:19:36 -0300

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardAvatarComponent } from './card-avatar/card-avatar.component';
+import { DiretivasComponent } from './diretivas/diretivas.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardAvatarComponent,
+    DiretivasComponent,
+    TemplateFormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
